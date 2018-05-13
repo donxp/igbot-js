cd3ba357f0a5b9871247202570a47ca6471e792c
Don <donshius@inbox.lt>
2018-05-13 06:14:08 +0100

    Added all account loading. Added proxies for accounts. Added the ability to get relationships between users.

--- src/Account.ts
+++ src/Account.ts
@@ -1,21 +1,79 @@
-import knex from "./Database"
+import knex from "./Database";
+import {V1 as Client} from "instagram-private-api";
+import Proxy from "./Proxy";
 
 export default class Account {
     static All: Account[] = [];
 
     username: string;
     password: string;
+    proxy: Proxy;
+    cookieMemoryStorage: Client.CookieMemoryStorage;
 
     constructor(username: string, password: string) {
         this.username = username;
         this.password = password;
+
+        this.cookieMemoryStorage = null;
     }
 
+    public attachProxy(proxy: Proxy) {
+        this.proxy = proxy;
+    }
+
+    public getRelationship(user: string) {
+        return new Promise((resolve, reject) => {
+            this.createUserSession()
+                .then(function(session) {
+                    return [session, Client.Account.searchForUser(session, user)];
+                })
+                .spread(function(session, account) {
+                    return Client.Relationship.create(session, account.id);
+                })
+                .then(resolve)
+                .catch(reject);
+        })
+    }
+
+    private createUserSession() {
+        return Client.Session.create(this.createDevice(), this.cookieMemoryStorage, this.username, this.password, `http://${this.proxy.ip}:${this.proxy.port}/`)
+    }
+
+    /**
+     * Create an emulated device based on username as the seed.
+     *
+     * @return {Client.Device}
+     */
+    private createDevice() {
+        return new Client.Device(this.username);
+    }
+
+    /**
+     * Create a memory-based cookie storage.
+     */
+    public createCookieStorage() {
+        this.cookieMemoryStorage = new Client.CookieMemoryStorage();
+    }
+
+    /**
+     * Desotry the memory-based cookie storage.
+     */
+    public destroyCookieStorage() {
+        if(this.cookieMemoryStorage != null) this.cookieMemoryStorage.destroy();
+    }
+
+    /**
+     * Load all accounts from the database.
+     *
+     * @return {Promise<any>}
+     */
     static loadAll() {
         return new Promise(function (resolve, reject) {
             knex.select().table('accounts')
                 .then(function(collection) {
-                    // Load and parse results
+                    collection.forEach(function(account) {
+                        Account.All.push(new Account(account.username, account.password));
+                    });
                     resolve();
                 })
                 .catch(function() {
--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,18 @@
 import Proxy from "./Proxy"
 import knex from "./Database"
+import Account from "./Account"
 
-knex.select().table("accounts")
-.then(function(collection) {
-    console.log(collection);
-});
+Account.loadAll().then(() => {
+    console.log('Number of accs:', Account.All.length);
+
+    let proxy = new Proxy(24000);
+    Account.All[3].createCookieStorage();
+    Account.All[3].attachProxy(proxy);
+    Account.All[3].getRelationship('drake')
+        .then(function(rel) {
+            console.log(rel);
+        })
+        .catch((err) => console.log(err));
+
+    console.log(Account.All[0].cookieMemoryStorage)
+})
